85d9d1dfc2dc5a7fa18876010c1428cfc829fe7d
oxamr <oxamr@proton.me>
2026-08-01 22:24:10 +0100

    Complete exercise 06-overflow

--- exercises/06-overflow/main.c
+++ exercises/06-overflow/main.c
@@ -1,8 +1,8 @@
 /*
  * ============================================================================
- * Exercise: 
- * Description: 
- * File: 
+ * Exercise: 06-overflow
+ * Description: Deliberately overflow a signed int; watch it wrap.
+ * File: main.c
  * Author: oxamr
  *
  * Copyright (c) 2026 oxamr.
@@ -13,10 +13,15 @@
  * ============================================================================
  */
  
- #include <stdio.h>
- 
- int main(void) {
-     // Write solution here
-     
-     return 0;
- }
+#include <stdio.h>
+
+int main(void) {
+    // Write solution here
+    int signed_min = -2147483648;
+    int signed_max = 2147483647;
+    
+    printf("%d\n", signed_min - 1);
+    printf("%d\n", signed_max + 1);
+    
+    return 0;
+}
