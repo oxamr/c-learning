8950a843fc950d91c11f57a9a1fe891b6af385ed
oxamr <oxamr@proton.me>
2026-08-01 19:53:56 +0100

    Add C exercise structure

---
+++ exercises/91-generic-vector/main.c
@@ -0,0 +1,22 @@
+/*
+ * ============================================================================
+ * Exercise: 
+ * Description: 
+ * File: 
+ * Author: oxamr
+ *
+ * Copyright (c) 2026 oxamr.
+ * All rights reserved.
+ *
+ * This source code is part of my personal C learning journey.
+ * Please do not submit this work as your own.
+ * ============================================================================
+ */
+ 
+ #include <stdio.h>
+ 
+ int main(void) {
+     // Write solution here
+     
+     return 0;
+ }
