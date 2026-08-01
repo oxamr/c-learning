b2cedb13b5758d6e67d3f3f43518937343b7d5fb
oxamr <oxamr@proton.me>
2026-08-01 22:44:06 +0100

    Complete exercise 07-conditionals

--- exercises/07-conditionals/main.c
+++ exercises/07-conditionals/main.c
@@ -1,8 +1,8 @@
 /*
  * ============================================================================
- * Exercise: 
- * Description: 
- * File: 
+ * Exercise: 07-conditionals
+ * Description: Classify a number: negative/zero/positive, even/odd.
+ * File: main.c
  * Author: oxamr
  *
  * Copyright (c) 2026 oxamr.
@@ -13,10 +13,31 @@
  * ============================================================================
  */
  
- #include <stdio.h>
- 
- int main(void) {
-     // Write solution here
-     
-     return 0;
- }
+#include <stdio.h>
+
+int main(void) {
+    // Write solution here
+    int input;
+    
+    printf("Enter integer: ");
+    if (scanf("%d", &input) != 1) {
+        printf("Error: invalid input\n");
+        return 1;
+    }
+    
+    if (input < 0) {
+        printf("%d is negative ", input);
+    } else if (input == 0) {
+        printf("%d is zero ", input);
+    } else {
+        printf("%d is positive ", input);
+    }
+    
+    if (input % 2 == 0) {
+        printf("and even\n", input);
+    } else {
+        printf("and odd\n", input);
+    }
+    
+    return 0;
+}
