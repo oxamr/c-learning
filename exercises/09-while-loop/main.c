a94b3309849d21cc5c0fef1ebd9326a36eba168f
oxamr <oxamr@proton.me>
2026-08-01 23:05:32 +0100

    Complete exercise 09-while-loop

--- exercises/09-while-loop/main.c
+++ exercises/09-while-loop/main.c
@@ -1,8 +1,8 @@
 /*
  * ============================================================================
- * Exercise: 
- * Description: 
- * File: 
+ * Exercise: 09-while-loop
+ * Description: Sum integers from user until they enter 0.
+ * File: main.c
  * Author: oxamr
  *
  * Copyright (c) 2026 oxamr.
@@ -13,10 +13,24 @@
  * ============================================================================
  */
  
- #include <stdio.h>
- 
- int main(void) {
-     // Write solution here
-     
-     return 0;
- }
+#include <stdio.h>
+
+int main(void) {
+    // Write solution here
+    int input = 1;
+    int sum = 0;
+    
+    while (input != 0) {
+        printf("Enter integer: ");
+        if (scanf("%d", &input) != 1) {
+            printf("Error: invalid input\n");
+            return 1;
+        }
+        
+        sum += input;
+    }
+    
+    printf("Total = %d\n", sum);
+    
+    return 0;
+}
