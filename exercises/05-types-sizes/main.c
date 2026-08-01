f3d4d60a561000a0b5f7180835be4a1be67ed76e
oxamr <oxamr@proton.me>
2026-08-01 22:02:33 +0100

    Complete exercise 05-types-sizes

--- exercises/05-types-sizes/main.c
+++ exercises/05-types-sizes/main.c
@@ -1,8 +1,8 @@
 /*
  * ============================================================================
- * Exercise: 
- * Description: 
- * File: 
+ * Exercise: 05-types-sizes
+ * Description: Print `sizeof` every primitive type with %zu.
+ * File: main.c
  * Author: oxamr
  *
  * Copyright (c) 2026 oxamr.
@@ -13,10 +13,15 @@
  * ============================================================================
  */
  
- #include <stdio.h>
- 
- int main(void) {
-     // Write solution here
-     
-     return 0;
- }
+#include <stdio.h>
+
+int main(void) {
+    // Write solution here
+    printf("Size of int    : %zu\n", sizeof(int));
+    printf("Size of long   : %zu\n", sizeof(long));
+    printf("Size of float  : %zu\n", sizeof(float));
+    printf("Size of double : %zu\n", sizeof(double));
+    printf("Size of char   : %zu\n", sizeof(char));
+        
+    return 0;
+}
