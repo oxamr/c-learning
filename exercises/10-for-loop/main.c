4849e5ff0acf19df04f368332dbb54eddc0eb10e
oxamr <oxamr@proton.me>
2026-08-01 23:14:34 +0100

    Complete exercise 10-for-loop

--- exercises/10-for-loop/main.c
+++ exercises/10-for-loop/main.c
@@ -1,8 +1,8 @@
 /*
  * ============================================================================
- * Exercise: 
- * Description: 
- * File: 
+ * Exercise: 10-for-loop
+ * Description: Print a multiplication table using a for loop.
+ * File: main.c
  * Author: oxamr
  *
  * Copyright (c) 2026 oxamr.
@@ -13,10 +13,19 @@
  * ============================================================================
  */
  
- #include <stdio.h>
- 
- int main(void) {
-     // Write solution here
-     
-     return 0;
- }
+#include <stdio.h>
+
+int main(void) {
+    // Write solution here
+    for (int x = 1; x <= 10; x++) {
+        printf("\n");
+        for (int y = 1; y <= 10; y++) {
+            int sum = x * y;
+            printf("%d ", sum);
+        }
+    }
+    
+    printf("\n");
+    
+    return 0;
+}
