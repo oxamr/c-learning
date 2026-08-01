abbb2146970dd15126683447aae47357832f79d5
oxamr <oxamr@proton.me>
2026-08-01 22:51:33 +0100

    Complete exercise 08-fizzbuzz

--- exercises/08-fizzbuzz/main.c
+++ exercises/08-fizzbuzz/main.c
@@ -1,8 +1,8 @@
 /*
  * ============================================================================
- * Exercise: 
- * Description: 
- * File: 
+ * Exercise: 08-fizzbuzz
+ * Description: Print numbers from 1 to 100 using the FizzBuzz rules.
+ * File: main.c
  * Author: oxamr
  *
  * Copyright (c) 2026 oxamr.
@@ -17,6 +17,17 @@
  
  int main(void) {
      // Write solution here
-     
+     for (int x = 1; x <= 100; x++) {
+         if (x % 3 == 0 && x % 5 == 0) {
+             printf("fizzbuzz\n");
+         } else if (x % 5 == 0) {
+             printf("buzz\n");
+         } else if (x % 3 == 0) {
+             printf("fizz\n");
+         } else {
+             printf("%d\n", x);
+         }
+     }
+          
      return 0;
  }
