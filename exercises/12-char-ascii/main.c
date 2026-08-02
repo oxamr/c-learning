36fc4b8d3c354513501a53df7f374b46b04685c9
oxamr <oxamr@proton.me>
2026-08-02 15:35:54 +0100

    Complete exercise 12-char-ascii

--- exercises/12-char-ascii/main.c
+++ exercises/12-char-ascii/main.c
@@ -1,8 +1,8 @@
 /*
  * ============================================================================
- * Exercise: 
- * Description: 
- * File: 
+ * Exercise: 12-char-ascii
+ * Description: Print each char A-Z with its ASCII value.
+ * File: main.c
  * Author: oxamr
  *
  * Copyright (c) 2026 oxamr.
@@ -13,10 +13,26 @@
  * ============================================================================
  */
  
- #include <stdio.h>
- 
- int main(void) {
-     // Write solution here
-     
-     return 0;
- }
+#include <stdio.h>
+
+int main(void) {
+    // Write solution here
+    char alphabet[] = {
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+        'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+        'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+        'Y', 'Z'
+    };
+    
+    for (int x = 0; x < sizeof(alphabet); x++) {
+        printf("%c = %d\n", alphabet[x], alphabet[x]);
+    }
+    
+    printf("\n");
+    
+    for (char c = 'A'; c <= 'Z'; c++) {
+        printf("%d = %c\n", c, c);
+    }
+    
+    return 0;
+}
