4ee1f0b8bc8486ffe4dccba3a45869b0a3a1bfe1
oxamr <oxamr@proton.me>
2026-08-02 22:33:16 +0100

    Complete exercise 14-array-minmax

--- exercises/14-array-minmax/main.c
+++ exercises/14-array-minmax/main.c
@@ -1,8 +1,8 @@
 /*
  * ============================================================================
- * Exercise: 
- * Description: 
- * File: 
+ * Exercise: 14-array-minmax
+ * Description: Find min and max in one pass.
+ * File: main.c
  * Author: oxamr
  *
  * Copyright (c) 2026 oxamr.
@@ -12,11 +12,30 @@
  * Please do not submit this work as your own.
  * ============================================================================
  */
- 
- #include <stdio.h>
- 
- int main(void) {
-     // Write solution here
-     
-     return 0;
- }
+
+#include <stdio.h>
+
+int main(void) {
+    // Write solution here
+    int array[] = {24, -35, 36, 95, -12};
+    int min = array[0];
+    int max = array[0];
+    
+    printf("Array: ");
+    
+    for (int x = 0; x < sizeof(array) / sizeof(array[0]); x++) {        
+        printf("%d ", array[x]);
+        
+        if (min > array[x]) {
+            min = array[x];
+        }
+        
+        if (max < array[x]) {
+            max = array[x];
+        }
+    }
+    
+    printf("\nMin = %d\nMax = %d\n", min, max);
+    
+    return 0;
+}
