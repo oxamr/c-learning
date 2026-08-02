e4b0f6814a5ce0255ed24fa3b0a6e83aec7913a4
oxamr <oxamr@proton.me>
2026-08-02 15:19:59 +0100

    Incomplete exercise 11-nested-loops

--- exercises/11-nested-loops/main.c
+++ exercises/11-nested-loops/main.c
@@ -1,8 +1,8 @@
 /*
  * ============================================================================
- * Exercise: 
- * Description: 
- * File: 
+ * Exercise: 11-nested-loops
+ * Description:  Print a triangle and a diamond of asterisks.
+ * File: main.c
  * Author: oxamr
  *
  * Copyright (c) 2026 oxamr.
@@ -13,10 +13,53 @@
  * ============================================================================
  */
  
- #include <stdio.h>
- 
- int main(void) {
-     // Write solution here
-     
-     return 0;
- }
+#include <stdio.h>
+
+int main(void) {
+    // Write solution here    
+    printf("\nTriangle:\n");
+    
+    for (int x = 1; x <= 5; x++) {
+        printf("\n");
+        for (int y = 1; y <= x; y++) {
+            printf("*");
+        }
+    }
+    
+    printf("\n\nUpside down triangle:\n");
+    
+    for (int x = 1; x <= 5; x++) {
+        printf("\n");
+        for (int y = 5; y >= x; y--) {
+            printf("*");
+        }
+    }
+    
+    printf("\n\nFlipped triangle:\n");
+        
+    for (int x = 1; x <= 5; x++) {
+        printf("\n");
+        for (int y = 5; y > x; y--) {
+            printf(" ");
+        }
+        for (int z = 1; z <= x; z++) {
+            printf("*");
+        }
+    }
+    
+    printf("\n\nUpside down flipped triangle:\n");
+        
+    for (int x = 1; x <= 5; x++) {
+        printf("\n");
+        for (int y = 1; y < x; y++) {
+            printf(" ");
+        }
+        for (int z = 5; z >= x; z--) {
+            printf("*");
+        }
+    }
+    
+    printf("\n");
+    
+    return 0;
+}
