453c9df979dc4c3eacdfa32f294c4bcbf21fb09a
oxamr <oxamr@proton.me>
2026-08-02 15:49:53 +0100

    Complete exercise 13-array-sum

--- exercises/13-array-sum/main.c
+++ exercises/13-array-sum/main.c
@@ -1,8 +1,8 @@
 /*
  * ============================================================================
- * Exercise: 
- * Description: 
- * File: 
+ * Exercise: 13-array-sum
+ * Description: Sum a fixed array; compute the average.
+ * File: main.c
  * Author: oxamr
  *
  * Copyright (c) 2026 oxamr.
@@ -13,10 +13,24 @@
  * ============================================================================
  */
  
- #include <stdio.h>
- 
- int main(void) {
-     // Write solution here
-     
-     return 0;
- }
+#include <stdio.h>
+
+int main(void) {
+    // Write solution here
+    int array[] = {10, 20, 30, 40, 50};
+    int sum = 0;
+    
+    printf("Array: ");
+    
+    for (int x = 0; x < sizeof(array) / sizeof(array[0]); x++) {
+        printf("%d ", array[x]);
+        sum += array[x];
+    }
+    
+    int average = sum / (sizeof(array) / sizeof(array[0]));
+    
+    printf("\nSum = %d\n", sum);
+    printf("Average = %d\n", average);
+    
+    return 0;
+}
