d65046a3ed811f7ca3c51165aae2a220e8e0819f
oxamr <oxamr@proton.me>
2026-08-03 17:38:59 +0100

    Complete exercise 16-array-search

--- exercises/16-array-search/main.c
+++ exercises/16-array-search/main.c
@@ -1,8 +1,8 @@
 /*
  * ============================================================================
- * Exercise: 
- * Description: 
- * File: 
+ * Exercise: 16-array-search
+ * Description:  Linear search; return index or -1.
+ * File: main.c
  * Author: oxamr
  *
  * Copyright (c) 2026 oxamr.
@@ -12,11 +12,43 @@
  * Please do not submit this work as your own.
  * ============================================================================
  */
- 
- #include <stdio.h>
- 
- int main(void) {
-     // Write solution here
-     
-     return 0;
- }
+
+#include <stdio.h>
+
+int main(void) {
+    // Write solution here
+    int array[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+    size_t array_size = sizeof(array) / sizeof(array[0]);
+    int user_choice;
+    int found = 0;
+    
+    printf("LINEAR SEARCH\n");
+    printf("Array: ");
+    
+    for (int x = 0; x < array_size; x++) {
+        printf("%d ", array[x]);
+    }
+    
+    printf("\n");
+    printf("Search: ");
+    
+    if (scanf("%d", &user_choice) != 1) {
+        printf("Error: invalid input\n");
+        return 1;
+    }
+    
+    for (int y = 0; y < array_size; y++) {
+        if (user_choice == array[y]) {
+            found = 1;
+            printf("Found %d at index %d\n", user_choice, y);
+            
+            return 0;
+        }
+    }
+    
+    if (!found) {
+        printf("%d not found\n", user_choice);
+    }
+    
+    return 0;
+}
