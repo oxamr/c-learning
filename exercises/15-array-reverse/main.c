0a17085ca8754ab9782782490fdd4efa7e662db9
oxamr <oxamr@proton.me>
2026-08-03 16:48:14 +0100

    Complete exercise 15-array-reverse && amended .gitignore

--- exercises/15-array-reverse/main.c
+++ exercises/15-array-reverse/main.c
@@ -17,7 +17,30 @@
 
 int main(void) {
     // Write solution here
+    int array[] = {10, 20, 30, 40, 50};
+    size_t array_size = sizeof(array) / sizeof(array[0]);
     
+    int temp = 0;
+    int left = 0;
+    int right = array_size - 1;
+    
+    while (left < right) {
+        temp = array[left];
+        
+        array[left] = array[right];
+        array[right] = temp;
+        
+        left++;
+        right--;
+    }
+    
+    printf("Reversed array: ");
+    
+    for (int x = 0; x < array_size; x++) {
+        printf("%d ", array[x]);
+    }
+    
+    printf("\n");
     
     return 0;
 }
