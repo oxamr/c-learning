1c1eae9bd90ded6e112a9d0ea7d724ad6de8424d
oxamr <oxamr@proton.me>
2026-08-04 16:17:14 +0100

    Complete exercise 18-bubble-sort

--- exercises/18-bubble-sort/main.c
+++ exercises/18-bubble-sort/main.c
@@ -1,8 +1,8 @@
 /*
  * ============================================================================
- * Exercise: 
- * Description: 
- * File: 
+ * Exercise: 18-bubble-sort
+ * Description: Implement bubble sort.
+ * File: main.c
  * Author: oxamr
  *
  * Copyright (c) 2026 oxamr.
@@ -12,11 +12,41 @@
  * Please do not submit this work as your own.
  * ============================================================================
  */
- 
- #include <stdio.h>
- 
- int main(void) {
-     // Write solution here
-     
-     return 0;
- }
+
+#include <stdio.h>
+
+int main(void) {
+    // Write solution here
+    int arr[] = {10, 50, -5, 40, 25, 80, 100, 29};
+    size_t arr_size = sizeof(arr) / sizeof(arr[0]);
+    
+    int temp = 0;
+    
+    printf("Array: ");
+    
+    for (int i = 0; i < arr_size; i++) {
+        printf("%d ", arr[i]);
+    }
+    
+    // Bubble sort
+    for (size_t pass = 0; pass < arr_size - 1; pass++) {
+        for (size_t i = 0; i < arr_size - 1 - pass; i++) {
+            if (arr[i] > arr[i - 1]) {
+                temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
+        }
+    }
+    
+    printf("\n");
+    printf("Bubble sorted array: ");
+    
+    for (size_t i = 0; i < arr_size; i++) {
+        printf("%d ", arr[i]);
+    }
+    
+    printf("\n");
+    
+    return 0;
+}
